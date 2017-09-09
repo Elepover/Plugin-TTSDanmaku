8293bdac467cf0965fc20e647696d5d860513c65
Elepover <elepover@outlook.com>
2017-09-09 21:39:39 +0800

    update KruinUpdates

--- TTSDanmaku/Window_Administration.xaml.vb
+++ TTSDanmaku/Window_Administration.xaml.vb
@@ -174,23 +174,8 @@ Public Class Window_Administration
     End Sub
 
     Private Sub Button_CheckUpdates_Click(sender As Object, e As Windows.RoutedEventArgs) Handles Button_CheckUpdates.Click
-        Try
-            'Check release
-            Dim latest As KruinUpdates.Update = KruinUpdates.GetLatestUpd()
-            Dim currVer As Version = New Version(New Main().PluginVer)
-
-            If Not KruinUpdates.CheckIfLatest(latest, currVer) Then
-                If MsgBox("最新版本: " & latest.LatestVersion.ToString & " 已发布！" & vbCrLf & "更新时间: " & latest.UpdateTime.ToLongDateString & " " & latest.UpdateTime.ToShortTimeString & vbCrLf & vbCrLf & "更新内容: " & latest.UpdateDescription & vbCrLf & vbCrLf & "是否前往下载？", MsgBoxStyle.Information + MsgBoxStyle.YesNo, "KruinUpdates") = MsgBoxResult.Yes Then
-                    Dim proc As New Process()
-                    proc.StartInfo.FileName = "https://www.danmuji.cn/plugins/TTSDanmaku"
-                    proc.Start()
-                End If
-            Else
-                NBlockMsgBox("插件已为最新(" & currVer.ToString & ")。" & vbCrLf & "更新时间: " & latest.UpdateTime.ToLongDateString & " " & latest.UpdateTime.ToShortTimeString, MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "KruinUpdates")
-            End If
-        Catch ex As Exception
-            NBlockMsgBox("检查更新时出错: " & ex.ToString, MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "KruinUpdates")
-        End Try
+        Dim frm As New Window_Upgrader
+        frm.Show()
     End Sub
 
     Private Sub Button_Close_Click(sender As Object, e As Windows.RoutedEventArgs) Handles Button_Close.Click
---
+++ TTSDanmaku/Window_Upgrader.xaml.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Windows
+
+Public Class Window_Upgrader
+    Private PluginDLURL As String = "undefined"
+
+    Private Sub Button_CheckUpd_Click(sender As Object, e As Windows.RoutedEventArgs) Handles Button_CheckUpd.Click
+        Me.IsEnabled = False
+        TextBlock_Status.Text = "正在检查更新..."
+        ProgressBar_Indicator.Visibility = Visibility.Visible
+        Delay(1000)
+
+        Dim latest As KruinUpdates.Update
+        Dim currVer As Version = New Version(New Main().PluginVer)
+        Try
+            'Check release
+            latest = KruinUpdates.GetLatestUpd()
+            TextBlock_Latest.Text = "最新版本: " & latest.LatestVersion.ToString
+            If KruinUpdates.CheckIfLatest(latest, currVer) Then
+                TextBlock_Status.Text = "插件已为最新。"
+            Else
+                TextBlock_Status.Text = "最新版本: " & latest.LatestVersion.ToString & " / 当前版本: " & currVer.ToString
+            End If
+            TextBox_UpdContents.Text = "更新时间: " & latest.UpdateTime.ToString() & vbCrLf & "更新日志: " & vbCrLf & latest.UpdateDescription
+        Catch ex As Exception
+            TextBlock_Status.Text = "检查更新时出错。"
+            TextBox_UpdContents.Text = "检查更新时出错: " & ex.ToString
+        End Try
+
+        ProgressBar_Indicator.Visibility = Visibility.Hidden
+        Me.IsEnabled = True
+    End Sub
+
+    Private Sub Window_Upgrader_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Me.Icon = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(My.Resources.icon.GetHbitmap, IntPtr.Zero, Int32Rect.Empty, Media.Imaging.BitmapSizeOptions.FromEmptyOptions())
+        Button_CheckUpd_Click(Nothing, Nothing)
+    End Sub
+
+    Private Sub Button_DLUpd_Click(sender As Object, e As RoutedEventArgs) Handles Button_DLUpd.Click
+        If PluginDLURL = "undefined" Then
+            MessageBox.Show("先检查更新吧！", "KruinUpdates", MessageBoxButton.OK, MessageBoxImage.Information)
+        Else
+            Process.Start(PluginDLURL)
+        End If
+    End Sub
+End Class
