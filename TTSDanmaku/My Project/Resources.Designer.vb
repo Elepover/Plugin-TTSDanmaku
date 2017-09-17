7cdc05064e756165847a07f7adfff545f2f844eb
Elepover <elepover@outlook.com>
2017-09-17 12:35:14 +0800

    switched to NAudio nuget and added ILMerge

--- TTSDanmaku/My Project/Resources.Designer.vb
+++ TTSDanmaku/My Project/Resources.Designer.vb
@@ -101,16 +101,6 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  查找 System.Byte[] 类型的本地化资源。
-        '''</summary>
-        Friend ReadOnly Property libNAudio() As Byte()
-            Get
-                Dim obj As Object = ResourceManager.GetObject("libNAudio", resourceCulture)
-                Return CType(obj,Byte())
-            End Get
-        End Property
-        
-        '''<summary>
         '''  查找 System.Drawing.Bitmap 类型的本地化资源。
         '''</summary>
         Friend ReadOnly Property mgmt() As System.Drawing.Bitmap
